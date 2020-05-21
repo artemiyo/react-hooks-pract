1119ca63e3cafd16e5fbb0ba0a70cb3924fbb99f
Artem <50479756+artemiyo@users.noreply.github.com>
2020-05-22 00:59:58 +0300

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useCallback } from "react";
 import Counter from "./Counter";
 import Greeting from "./Greeting";
+import CounterReducer from "./CounterReducer";
 
 function App() {
   return (
     <div className="App">
       <Counter />
       <Greeting />
+      <CounterReducer />
     </div>
   );
 }
